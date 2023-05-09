cc3d26caddf2d5a9283108ea58baf27b923dff21
Saqeb Deshmukh <saqeb.deshmukh@writerinformation.com>
2023-05-09 15:22:28 +0530

    project setup

---
+++ BLL/Transactions/VendorPortal/InvoiceSubmission/InvoiceReportBLL.cs
@@ -0,0 +1,15 @@
+﻿using DLL;
+using DLL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Transactions.VendorPortal.InvoiceSubmission
+{
+    public class InvoiceReportBLL : GenericRepository<TBL_BarcodeGen_Old>
+    {
+      
+    }
+}
---
+++ DLL/GenericeRepository.cs
@@ -0,0 +1,52 @@
+﻿using DLL.Repos;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+
+    public class GenericRepository<T> where T : class
+    {
+        public InvoiceProcessingEntities DbContext = new InvoiceProcessingEntities();
+        private readonly DbSet<T> DbSet;
+
+        public IQueryable GetWithoutTracking(object id)
+        {
+            return DbSet.AsNoTracking();
+        }
+
+        public IQueryable GetWithTracking(object id)
+        {
+            return DbSet;
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            var insertedEntity = DbSet.Add(entity);
+            await DbContext.SaveChangesAsync();
+            return insertedEntity;
+        }
+
+        public async Task<T> Update(T entityToUpdate)
+        {
+            DbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            await DbContext.SaveChangesAsync();
+            return entityToUpdate;
+        }
+
+        public async Task<T> Delete(T entityToDelete)
+        {
+            if (DbContext.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Attach(entityToDelete);
+            }
+
+            DbSet.Remove(entityToDelete);
+            await DbContext.SaveChangesAsync();
+
+            return entityToDelete;
+        }
+    }
+
+}
