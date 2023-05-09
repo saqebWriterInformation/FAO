3012783d656bad85433d3631adb748ac29b8687c
Bhagyashree Damale <bhagyashree.damale@writerinformation.com>
2023-05-09 16:19:24 +0530

    Test success

--- BLL/Transactions/VendorPortal/InvoiceSubmission/InvoiceReportBLL.cs
+++ BLL/Transactions/VendorPortal/InvoiceSubmission/InvoiceReportBLL.cs
@@ -2,14 +2,18 @@
 using DLL.Repos;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL.Transactions.VendorPortal.InvoiceSubmission
 {
-    public class InvoiceReportBLL : GenericRepository<TBL_BarcodeGen_Old>
+    public class InvoiceReportBLL : GenericRepository<SYS_USERMASTER>
     {
-      
+      public async Task<List<SYS_USERMASTER>> GetAlluser()
+        {
+           return await DbContext.SYS_USERMASTER.ToListAsync();
+        }
     }
 }
