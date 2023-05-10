7274d43e40d285bd24411f66c082c24cc00e71fe
saqebWriterInformation <132999217+saqebWriterInformation@users.noreply.github.com>
2023-05-10 15:24:26 +0530

    added mail service for sending email (#2)

---
+++ BLL/Services/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class EmailService
+    {
+        public bool SendMail(string toEmails, string subject, string emailBody, bool isBodyHtml = true)
+        {
+            bool mailStatus = true;
+
+            try
+            {
+                var mailClient = GetConfiguredSMTPClient();
+
+                //---------EMail Sending Coade -----------------  //
+                MailMessage mailMessage = new MailMessage(ConfigurationSettings.AppSettings["FromMailID"].ToString(), toEmails);
+                mailMessage.Subject = subject;
+                //--------------Mail Body----------------------------//
+                mailMessage.IsBodyHtml = isBodyHtml;
+                mailMessage.Body = emailBody;
+
+                mailClient.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                mailStatus = false;
+            }
+            return mailStatus;
+        }
+
+        private SmtpClient GetConfiguredSMTPClient()
+        {
+            var a = ConfigurationSettings.AppSettings["MailHost"];
+            SmtpClient mailClient = new SmtpClient(ConfigurationSettings.AppSettings["MailHost"].ToString(),
+                Convert.ToInt32(ConfigurationSettings.AppSettings["MailPort"]));
+
+            System.Net.NetworkCredential basicAuthenticationInfo = new System.Net.NetworkCredential(ConfigurationSettings.AppSettings["MailUserID"].ToString(),
+                ConfigurationSettings.AppSettings["MailPassword"].ToString());
+            mailClient.Credentials = basicAuthenticationInfo;
+
+            return mailClient;
+        }
+    }
+}
--- BLL/Transactions/VendorPortal/InvoiceSubmission/InvoiceReportBLL.cs
+++ BLL/Transactions/VendorPortal/InvoiceSubmission/InvoiceReportBLL.cs
@@ -1,11 +1,15 @@
-﻿using DLL;
+﻿using BLL.Services;
+using DLL;
 using DLL.Repos;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Hosting;
 
 namespace BLL.Transactions.VendorPortal.InvoiceSubmission
 {
@@ -13,7 +17,22 @@ namespace BLL.Transactions.VendorPortal.InvoiceSubmission
     {
       public async Task<List<SYS_USERMASTER>> GetAlluser()
         {
+           SendEmail();
            return await DbContext.SYS_USERMASTER.ToListAsync();
+        }
+
+        public void SendEmail()
+        {
+
+            EmailService emailService = new EmailService();
+            var reader = new StreamReader($@"{HostingEnvironment.MapPath("~")}\Templates\LoginCredential.htm");
+            string readFile = reader.ReadToEnd();
+
+            readFile = readFile.Replace("$$empname$$", "Saqeb Deshmukh");
+            readFile = readFile.Replace("$$loginid$$", "V000001");
+            readFile = readFile.Replace("$$pwd$$", "123456");
+
+            emailService.SendMail("saqeb.deshmukh@writerinformation.com", "Login Credential", readFile);
         }
     }
 }
